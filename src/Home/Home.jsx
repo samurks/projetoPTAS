e42cba33552005a235369089c011ae0be471de2d
samurks <samueltorochagas@gmail.com>
2023-09-01 21:34:00 -0300

    refazendo pq o code deu erro

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import "./style.css";
 export default function Home() {
    
     return (
         <div>
-            <h1>Home</h1>
-            <Link to="/todo">todo</Link>
+            <h1>BEM VINDO</h1>
+            <Link to="/todo">Ir para lista de planetas</Link>
         </div>
 
     );
--- src/Todo/ToDo.jsx
+++ src/Todo/ToDo.jsx
@@ -28,9 +28,9 @@ export default function ToDo() {
     }
     return (
         <div class="container">
-            <Link to="/">home</Link>
-            <h1>Lista de Atividades</h1>
-            <form class="input" onSubmit={salvar}>
+            <Link class="links"to="/">Voltar para pagina principal</Link>
+            <h1>Lista de Planetas</h1>
+            <form onSubmit={salvar}>
                 <input type="text"
                     value={atividade}
                     onChange={(e) => { setAtividade(e.target.value) }} />
@@ -38,9 +38,8 @@ export default function ToDo() {
             </form>
             {lista.map((ativ) =>
                 <ul key={ativ.id}>
-                    <li>
-                    <p>{ativ.id}</p>
-                        <p>{ativ.atividade}</p>
+                    <li class="idPlanetas">
+                        <p>{ativ.id} {ativ.atividade}</p>
                         <button onClick={() => remover(ativ.id)}>Remover</button>
                     </li>
                 </ul>
