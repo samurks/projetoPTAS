3b6a4581743496aac1ce350e10b322c1242361ac
samurks <samueltorochagas@gmail.com>
2023-09-11 21:22:00 -0300

    apenas estilizado, nao adicionei nenhuma funcionalidade nova

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,14 +1,14 @@
-import React from "react";
+import { useState } from "react";
 
 import { Link } from "react-router-dom";
 
 export default function Home() {
    
     return (
-        <div>
-            <h1>Home</h1>
-            <Link to="/todo">Ir para proxima pagina</Link>
+        <div class="containerHome">
+            <h1>Agenda planetaria</h1>
+            <p>Explore o fascinante mundo dos planetas e da astrologia!</p>
+            <Link class="linkHome" to="/todo">Ir para proxima pagina</Link>
         </div>
-
     );
 }
--- src/Todo/ToDo.jsx
+++ src/Todo/ToDo.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import "./style.css";
+import "./style.css"; // Importe o arquivo de estilos
 
 export default function ToDo() {
     const [atividade, setAtividade] = useState("");
@@ -16,36 +16,36 @@ export default function ToDo() {
         setId(id + 1);
         setAtividade("");
     };
+
     const remover = (id) => {
-        /*setLista(lista.filter((ativ) => (ativ.id !== id ? lista : null)));*/
-        const auxLista = [];
-        lista.map((lista) => {
-            if (lista.id !== id) {
-                auxLista.push(lista);
-            }
-        });
+        const auxLista = lista.filter((ativ) => ativ.id !== id);
         setLista(auxLista);
     }
+
     return (
-        <div class="container">
-            <div className="link-container">
-            <Link class="links"to="/">Voltar para pagina principal</Link>
-            </div>
+        <div class="lista-planetas">
+           <Link to="/" class="link-home">Voltar para a página principal</Link>
             <h1>Lista de Planetas</h1>
-            <form  class="inputPlaneta"onSubmit={salvar}>
-                <input type="text"
-                    value={atividade}
-                    onChange={(e) => { setAtividade(e.target.value) }} />
-                <button>ADICIONAR</button>
-            </form>
-            {lista.map((ativ) =>
+            <div class="form-container">
+                <form onSubmit={salvar}>
+                    <input
+                        type="text"
+                        value={atividade}
+                        onChange={(e) => setAtividade(e.target.value)}
+                    />
+                    <button>ADICIONAR</button>
+                </form>
+            </div>
+            {lista.map((ativ) => (
                 <ul key={ativ.id}>
-                    <li class="idPlanetas">
+                    <li>
                         <p>{ativ.id} {ativ.atividade}</p>
-                        <button onClick={() => remover(ativ.id)}>Remover</button>
+                        <button class="remove-button" onClick={() => remover(ativ.id)}>
+                            Remover
+                        </button>
                     </li>
                 </ul>
-            )}
+            ))}
         </div>
     );
 }
