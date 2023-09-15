e47f767f2817a45f31f789fc9522985e34ee43ab
samurks <samueltorochagas@gmail.com>
2023-09-15 17:29:03 -0400

    mechi no home, css e no todo
    fiz alterações basicas no home,troquei as classes do todo e home adicionei um input de imagem com base de urls na web de img

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -5,11 +5,11 @@ import { Link } from "react-router-dom";
 export default function Home() {
    
     return (
-        <div class="containerHome">
-            <h1>Agenda planetaria</h1>
+        <div className="containerHome">
+            <h1>AGENDA PLANETARIA</h1>
             <p>Explore o fascinante mundo dos planetas e da astrologia!</p>
             <br></br>
-            <Link class="linkHome" to="/todo">Ir para proxima pagina</Link>
+            <Link className="link-agenda" to="/todo">Ir para proxima pagina</Link>
         </div>
     );
 }
--- src/Todo/ToDo.jsx
+++ src/Todo/ToDo.jsx
@@ -6,32 +6,50 @@ export default function ToDo() {
     const [atividade, setAtividade] = useState("");
     const [lista, setLista] = useState([]);
     const [id, setId] = useState(1);
+    const [imagem, setimagem] = useState("");
 
     const salvar = (e) => {
         e.preventDefault();
         setLista([...lista, {
             atividade: atividade,
-            id: id
+            id: id,
+            imagem:imagem
         }]);
         setId(id + 1);
         setAtividade("");
+        setimagem("");
+        
     };
 
     const remover = (id) => {
-        const auxLista = lista.filter((ativ) => ativ.id !== id);
+        /*setLista(lista.filter((ativ) => (ativ.id !== id ? lista : null)));*/
+        const auxLista = [];
+        lista.map((lista) => {
+            if (lista.id !== id) {
+                auxLista.push(lista);
+            }
+        });
         setLista(auxLista);
+ 
     }
 
     return (
-        <div class="lista-planetas">
-           <Link to="/" class="link-home">Voltar para a página principal</Link>
+        <div className="lista-planetas">
+           <Link to="/" className="link-home">Voltar para a página principal</Link>
             <h1>Lista de Planetas</h1>
-            <div class="form-container">
+            <div className="form-container">
                 <form onSubmit={salvar}>
                     <input
                         type="text"
                         value={atividade}
                         onChange={(e) => setAtividade(e.target.value)}
+                    />
+                    
+            
+                    <input
+                        type="text"
+                        value={imagem}
+                        onChange={(e) => setimagem(e.target.value)}
                     />
                     <button>ADICIONAR</button>
                 </form>
@@ -39,8 +57,10 @@ export default function ToDo() {
             {lista.map((ativ) => (
                 <ul key={ativ.id}>
                     <li>
-                        <p> {ativ.atividade}</p>
-                        <button class="remove-button" onClick={() => remover(ativ.id)}>
+                        <p className="idAtiv"> {ativ.id} {ativ.atividade} 
+                        </p>
+                        <img src={ativ.imagem}></img>
+                        <button className="remove-button" onClick={() => remover(ativ.id)}>
                             Remover
                         </button>
                     </li>
