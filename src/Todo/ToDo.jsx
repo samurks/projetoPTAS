9dbd9948334075254f1b85c2345db1c87e3fbf64
samurks <samueltorochagas@gmail.com>
2023-10-20 17:26:37 -0400

    tudo okay
    blz

--- src/Todo/ToDo.jsx
+++ src/Todo/ToDo.jsx
@@ -1,73 +1,74 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import "./style.css"; // Importe o arquivo de estilos
+import "./style.css";
 
 export default function ToDo() {
-    const [atividade, setAtividade] = useState("");
-    const [lista, setLista] = useState([]);
-    const [id, setId] = useState(1);
-    const [imagem, setimagem] = useState("");
+  const listaLocalStorage = JSON.parse(localStorage.getItem("lista")) || [];
+  const [atividade, setAtividade] = useState("");
+  const [lista, setLista] = useState(listaLocalStorage);
+  const [id, setId] = useState(1);
+  const [imagem, setImagem] = useState("");
 
-    const salvar = (e) => {
-        e.preventDefault();
-        setLista([...lista, {
-            atividade: atividade,
-            id: id,
-            imagem:imagem
-        }]);
-        console.log(lista)
-        setId(id + 1);
-        setAtividade("");
-        setimagem("");
+  useEffect(() => {
+    localStorage.setItem("lista", JSON.stringify(lista));
+  }, [lista]);
+
+  const salvar = (e) => {
+    e.preventDefault();
+    setLista([
+      ...lista,
+      {
+        atividade: atividade,
+        id: id,
+        imagem: imagem,
+      },
+    ]);
+    setId(id + 1);
+    setAtividade("");
+    setImagem("");
+  };
+
+  const remover = (id) => {
+    const novaLista = lista.filter((ativ) => ativ.id !== id);
+    setLista(novaLista);
+  };
+
+  return (
+    <div className="lista-planetas">
+       <Link to="/" className="link-home">Voltar para a página principal</Link>
+        <h1>Lista de Planetas</h1>
+        <div className="form-container">
+            <form onSubmit={salvar}>
+                <input
+                    type="text"
+                    value={atividade}
+                    onChange={(e) => setAtividade(e.target.value)}
+                    placeholder="Planeta"
+                />
+                
         
-    };
-
-    const remover = (id) => {
-        /*setLista(lista.filter((ativ) => (ativ.id !== id ? lista : null)));*/
-        const auxLista = [];
-        lista.map((lista) => {
-            if (lista.id !== id) {
-                auxLista.push(lista);
-            }
-        });
-        setLista(auxLista);
- 
-    }
-
-    return (
-        <div className="lista-planetas">
-           <Link to="/" className="link-home">Voltar para a página principal</Link>
-            <h1>Lista de Planetas</h1>
-            <div className="form-container">
-                <form onSubmit={salvar}>
-                    <input
-                        type="text"
-                        value={atividade}
-                        onChange={(e) => setAtividade(e.target.value)}
-                    />
-                    
-            
-                    <input
-                        type="text"
-                        value={imagem}
-                        onChange={(e) => setimagem(e.target.value)}
-                    />
-                    <button>ADICIONAR</button>
-                </form>
-            </div>
-            {lista.map((ativ) => (
-                <ul key={ativ.id}>
-                    <li>
-                        <p className="idAtiv"> {ativ.id} {ativ.atividade} 
-                        </p>
-                        <img className="Planeta"src={ativ.imagem}></img>
-                        <br></br>
-                        <button className="remove-button" onClick={() => remover(ativ.id)}>
-                            Remover
-                        </button>
-                    </li>
-                </ul>
-            ))}
+                <input
+                    type="text"
+                    value={imagem}
+                    onChange={(e) => setimagem(e.target.value)}
+                    placeholder="URL da Imagem"
+                />
+                <button>ADICIONAR</button>
+            </form>
         </div>
-    );
+        {lista.map((ativ) => (
+            <ul key={ativ.id}>
+                <li>
+                    <p className="idAtiv"> {ativ.id} {ativ.atividade} 
+                    </p>
+                    <img className="Planeta"src={ativ.imagem}></img>
+                    <br></br>
+                    <button className="remove-button" onClick={() => remover(ativ.id)}>
+                        Remover
+                    </button>
+                </li>
+            </ul>
+        ))}
+    </div>
+);
 }
